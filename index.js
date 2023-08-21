be4dce6a775d17f807f5fd1bd09d0f5d51a546ef
johney <johneyburke@gmail.com>
2023-08-21 14:15:43 -0600

    7/7 tests complete

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+
+const employee = {
+
+    name : "jimbo",
+    streetAddress: "123 Fake St",
+
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+
+   const  newEmployee = { ...obj };
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+
+    obj[key] = value;
+
+    return obj;
+
+}
+
+function deleteFromEmployeeByKey(obj, key){
+   
+    const  updatedEmployee = { ...obj };
+    delete updatedEmployee[key] ;
+
+    return updatedEmployee;
+
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key] ;
+    return obj;
+
+}
+
